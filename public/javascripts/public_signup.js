060ec07cd72071906c076726414d924b893e7dbe
Mike Park <mikep@quake.net>
2011-02-03 16:45:04 +0100

    dont focus on error field

--- public/javascripts/public_signup.js
+++ public/javascripts/public_signup.js
@@ -14,13 +14,3 @@ var pm = function() {
 
 $("input[name='public_signup[registration_attributes][payment_method]']").click(pm);
 pm();
-
-(function(){
-  var err = $('.error').first();
-  if (err.length==0) return;
-  var f = $('select', err)
-  if (f.length==0) {
-    f = $('input', err);
-  }
-  if (f) f.focus();
-})();
