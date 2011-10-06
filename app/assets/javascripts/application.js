b61a86bbbc14ffb15cc09b85e37d74c378b22792
Mike Park <git@t.quake.net>
2011-10-06 14:40:40 +0200

    moving js and css assets into app/assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,9 @@
 //= require jquery_ujs
 //= require_self
 //= require_tree .
+
+$(document).ready(function(){
+  var dp = $('input.ui-date-picker');
+  if (dp.length > 0)
+    dp.datepicker({ dateFormat: 'dd.mm.yy' });
+});
--- public/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-$(document).ready(function(){
-  var dp = $('input.ui-date-picker');
-  if (dp.length > 0)
-    dp.datepicker({ dateFormat: 'dd.mm.yy' });
-});
-
