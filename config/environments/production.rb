897f82abf25983c40f010da9035b49274d2b5244
Mike Park <git@t.quake.net>
2012-03-23 01:01:27 +0100

    use sendgrid for outgoing mail & config it this time!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,19 @@ Haidb::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :address => "smtp.sendgrid.net",
+      :port => 587,
+      :domain => 'haidb.info',
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
+
   # devise required
   config.action_mailer.default_url_options = { :host => ENV['HTTP_HOST'] }
 
--- config/initializers/site.rb
+++ config/initializers/site.rb
@@ -18,18 +18,6 @@ class Site
       end
     end
 
-    def smtp_settings
-      {
-        :user_name => ENV['SENDGRID_USERNAME'],
-        :password => ENV['SENDGRID_PASSWORD'],
-        :address => "smtp.sendgrid.net",
-        :port => 587,
-        :domain => 'haidb.info',
-        :authentication => :plain,
-        :enable_starttls_auto => true
-      }
-    end
-
     def default_country
       sites = {
         'de' => 'DE',
