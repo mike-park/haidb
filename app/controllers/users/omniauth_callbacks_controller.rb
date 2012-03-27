f47ea9da9b29662248f07b5e36ba7a414850b6e8
Mike Park <git@t.quake.net>
2012-03-27 18:00:25 +0200

    failed facebook feedback

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_session_url, alert: "Sorry, we don't have a record of you."
+      redirect_to new_user_session_url, alert: "Sorry, your facebook email address does not match our records."
     end
   end
 end
