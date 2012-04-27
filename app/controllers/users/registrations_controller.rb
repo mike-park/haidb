82c4cb6c0b7b9cf91a2fc0336b33b433b0924e73
Mike Park <git@t.quake.net>
2012-04-27 12:46:18 +0200

    return to sign-in page after sign-up

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,7 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def after_inactive_sign_up_path_for(resource)
+    user_root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Haidb::Application.routes.draw do
   devise_for :staffs	# office people
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+                                    registrations: 'users/registrations' }
 
   filter :locale
 
