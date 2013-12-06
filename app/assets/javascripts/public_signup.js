bfd16d9bb304e19df3418d1a319617a458d21914
Mike Park <mikep@quake.net>
2013-12-06 10:54:10 +0100

    tell parent we have resized for times we are within an iframe

--- app/assets/javascripts/public_signup.js
+++ app/assets/javascripts/public_signup.js
@@ -13,9 +13,17 @@ var pm = function() {
     jQuery('#internet').hide();
     jQuery('#post').hide();
   }
-}
+};
 
 jQuery(function(){
     jQuery("input[name='public_signup[registration_attributes][payment_method]']").click(pm);
     pm();
+});
+
+jQuery(function() {
+  if ((window.parent != window) && window.parent.postMessage) {
+    var height = jQuery('body').height();
+    var msg = "setHeight/" + height;
+    window.parent.postMessage(msg, '*');
+  }
 });
