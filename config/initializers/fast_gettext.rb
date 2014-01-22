65170200bd2d7c5f1fca9f6b99f3732eea83c944
Mike Park <mikep@quake.net>
2014-01-23 00:25:59 +0100

    updated rails to 3.2.16

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -2,7 +2,7 @@ require "fast_gettext/translation_repository/db"
 
 FastGettext::TranslationRepository::Db.require_models
 FastGettext.add_text_domain 'app', :type => :db, :model => TranslationKey
-FastGettext.default_available_locales = ['en','de'] #all you want to allow
+FastGettext.default_available_locales = ['en','de', 'en-GB'] #all you want to allow
 FastGettext.default_text_domain = 'app'
 
 # allow %{foo} substitutions to contain html
