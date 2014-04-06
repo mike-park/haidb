8bc06204e70823c1df406622d76642ff50de73c0
Mike Park <mikep@quake.net>
2014-04-06 14:03:20 +0200

    new capybara prefers features rather than requests

--- spec/features/office/dashboard_spec.rb
+++ spec/features/office/dashboard_spec.rb
@@ -8,6 +8,7 @@ describe "dashboard", js: true do
   it "should run web browser test" do
     visit office_dashboards_path
     expect(Staff.count).to eq(1)
+    # page.save_screenshot('test.png', full: true)
     expect(page).to have_link('Dashboard')
   end
 end
