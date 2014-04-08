423abe59a98258e63fe98f90fe144d8d3afcaad2
Mike Park <mikep@quake.net>
2014-04-09 01:04:20 +0200

    specify the csv fields to check

--- spec/models/concerns/csvable_spec.rb
+++ spec/models/concerns/csvable_spec.rb
@@ -15,8 +15,15 @@ describe Csvable do
 "Participant","Mike Park","name@example.com","","Street Address","plz","Berlin","DE","123","456","789","Cash","M Park","iban","bic","abc","100.0","20.0","80.0","pending","false","A long\nmessage\nhere"
 EOF
   end
+  let(:csv_fields) { [:role, :full_name, :email, :gender,
+                      :address, :postal_code, :city, :country,
+                      :home_phone, :mobile_phone, :work_phone,
+                      :payment_method, :bank_account_name, :iban, :bic, :registration_code,
+                      :cost, :paid, :owed,
+                      :status, :completed,
+                      :notes] }
 
   it "should convert array into csv" do
-    Registration.to_csv([reg]).should == csv
+    Registration.to_csv([reg], csv_fields).should == csv
   end
 end
