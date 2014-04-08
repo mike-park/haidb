b28482ca6fdf1426e5e2b1887267a207874dce5e
Mike Park <mikep@quake.net>
2014-04-08 18:11:48 +0200

    allow overriding default csv fields

--- app/models/concerns/csvable.rb
+++ app/models/concerns/csvable.rb
@@ -11,20 +11,20 @@ module Csvable
       @csv_fields = names
     end
 
-    def to_csv(array)
-      return unless @csv_fields
+    def to_csv(array, csv_fields = @csv_fields)
+      return unless csv_fields
       CSV.generate(:force_quotes => true, :encoding => 'utf-8') do |csv|
-        csv << csv_header
+        csv << csv_header(csv_fields)
         array.each do |a|
-          csv << @csv_fields.map {|f| a.send(f) }
+          csv << csv_fields.map {|f| a.send(f) }
         end
       end
     end
 
     private
 
-    def csv_header
-      @csv_fields.map { |f| f.to_s.humanize }
+    def csv_header(csv_fields)
+      csv_fields.map { |f| f.to_s.humanize }
     end
   end
 end
