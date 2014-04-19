c63babc8fa5be9c348e771e7b5ab573f6bd62929
Mike Park <mikep@quake.net>
2014-04-19 14:01:26 +0200

    verify nav links

--- spec/features/office/site_defaults_spec.rb
+++ spec/features/office/site_defaults_spec.rb
@@ -1,12 +1,28 @@
 require 'spec_helper'
 
-describe "site_defaults" do
+describe "site_defaults", js: true do
   before do
     office_login
+    visit office_root_path
   end
 
-  it "should have Emails tab" do
-    visit office_site_defaults_path
-    page.should have_content("Email Templates")
+  it "should have nav" do
+    expect(page).to have_content('Site Defaults')
+  end
+
+  context "nav links" do
+    before do
+      click_link('Site Defaults')
+    end
+
+    it "should have keys" do
+      click_link('Keys')
+      expect(page).to have_link('New')
+    end
+
+    it "should have emails" do
+      click_link('Emails')
+      expect(page).to have_link('New')
+    end
   end
 end
