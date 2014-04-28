5a045717e2f4adceb2597ef67c1834f6c217724d
Mike Park <mikep@quake.net>
2014-04-28 13:11:10 +0200

    must save record before we can verify geocoding updates

--- spec/models/concerns/mappable_spec.rb
+++ spec/models/concerns/mappable_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Mappable do
-  let(:reg) { build(:registration) }
+  let(:reg) { build(:registration, address: 'address', city: 'city') }
 
   context "normal test mode" do
     it "should not geocode" do
@@ -21,38 +21,42 @@ describe Mappable do
     end
 
     it 'should access the full_address on save' do
-      Gmaps4rails.should_receive(:geocode).and_return([{}])
       reg.should receive(:full_address)
-      reg.save!
-    end
-
-    it "should be geocoded?" do
-      reg.should_not be_geocoded
-      reg.lat = reg.lng = 1
-      reg.should be_geocoded
-    end
-
-    it "should not geocode" do
-      reg.lat = reg.lng = 1
-      reg.last_name = 'foo'
-      Gmaps4rails.should_not_receive(:geocode)
       reg.save
     end
 
-    it "should geocode" do
-      reg.lat = reg.lng = 1
-      Gmaps4rails.should_receive(:geocode).exactly(5).and_return([{}])
-      [:last_name, :address, :city, :postal_code, :country, :first_name].each do |name|
-        reg.send("#{name}=", name)
-        reg.save
+    context "with a geocoded registation" do
+      before do
+        reg.save!
       end
-    end
 
-    # it "should geocode to real values" do
-    #   # accesses google. might temporarily fail
-    #   reg.update_attributes(city: 'Berlin', country: 'DE')
-    #   reg.lat.should be_within(0.5).of(52.52)
-    #   reg.lng.should be_within(0.5).of(13.40)
-    # end
+      it "should be geocoded?" do
+        reg.should be_geocoded
+      end
+
+
+      it "should not geocode with non address changes" do
+        Gmaps4rails.should_not_receive(:geocode)
+        [:first_name, :last_name].each do |name|
+          reg.send("#{name}=", name)
+          reg.save
+        end
+      end
+
+      it "should geocode with address change" do
+        Gmaps4rails.should_receive(:geocode).exactly(4).and_return([{}])
+        [:address, :city, :postal_code, :country].each do |name|
+          reg.send("#{name}=", name)
+          reg.save
+        end
+      end
+
+      # it "should geocode to real values" do
+      #   # accesses google. might temporarily fail
+      #   reg.update_attributes(city: 'Berlin', country: 'DE')
+      #   reg.lat.should be_within(0.5).of(52.52)
+      #   reg.lng.should be_within(0.5).of(13.40)
+      # end
+    end
   end
 end
