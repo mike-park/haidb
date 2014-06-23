b61776855e1d3b5fba0d1e68cf7a9ce59433203d
Mike Park <mikep@quake.net>
2014-06-23 10:06:11 +0200

    refactor tests

--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -1,22 +1,18 @@
 # -*- coding: utf-8 -*-
-require "spec_helper"
+require 'spec_helper'
 
 describe Notifier do
-  context "#registration_with_template" do
-    let(:event) { FactoryGirl.build(:event, display_name: 'Event Name') }
-    let(:registration) { FactoryGirl.build(:registration, first_name: 'Mike', last_name: 'Park',
-                                           email: 'somewhere@example.com', event: event, cost: 1.23,
-                                           registration_code: 'reg_code', event: event,
-                                           iban: 'iban', bic: 'bic', bank_account_name: 'account name') }
+  let(:event) { FactoryGirl.build(:event, display_name: 'Event Name') }
+  let(:registration) { FactoryGirl.build(:registration, first_name: 'Mike', last_name: 'Park',
+                                         email: 'somewhere@example.com', event: event, cost: 1.23,
+                                         registration_code: 'reg_code', event: event,
+                                         iban: 'iban', bic: 'bic', bank_account_name: 'account name') }
+  let(:email) { FactoryGirl.build(:en_email) }
+  let(:options) { {} }
+  subject { Notifier.registration_with_template(registration, email, options) }
+
+  context 'mail headers' do
     let(:sender) { 'a_person@example.com' }
-    let(:interpolated_vars) { "{{person_name}} {{event_name}} {{registration_code}} {{iban}} {{bic}} {{account_name}} {{cost}}" }
-    let(:interpolated_result) { "#{registration.full_name} #{event.display_name} #{registration.registration_code} #{registration.iban} #{registration.bic} #{registration.bank_account_name}" }
-    let(:email) { FactoryGirl.build(:en_email,
-                                    subject: "subject #{interpolated_vars}",
-                                    body: "body #{interpolated_vars}") }
-
-    subject { Notifier.registration_with_template(registration, email) }
-
     before do
       SiteDefault.stub(:get).with('email.registrations.from_address').and_return(sender)
     end
@@ -24,35 +20,53 @@ describe Notifier do
     its(:from) { should == [sender] }
     its(:to) { should == [registration.email] }
     its(:bcc) { should == [sender] }
-    [
-        ['de', '1,23 €', '1,23 =E2=82=AC='],
-        ['uk', '£1.23', '=C2=A31.23=']
-    ].each do |(site, subject_cost, body_cost)|
-      it "should have #{site} cost subject" do
-        Site.stub(:name).and_return(site)
-        subject.subject.should match(subject_cost)
-      end
-      it "should have #{site} cost body" do
-        Site.stub(:name).and_return(site)
-        subject.encoded.should match(body_cost)
-      end
-    end
+  end
 
-    its(:subject) { should match("subject #{interpolated_result}") }
-    its(:encoded) { should match("body #{interpolated_result}") }
+  context 'override from and bcc' do
+    let(:from) { 'haha@override.com' }
+    let(:options) { {from: from} }
 
-    it "should deliver email" do
-      ActionMailer::Base.deliveries.count.should == 0
+    its(:from) { should == [from] }
+    its(:bcc) { should == [from] }
+  end
+
+  context 'delivery' do
+    it 'should deliver email' do
+      expect(ActionMailer::Base.deliveries.count).to eq(0)
       subject.deliver
-      ActionMailer::Base.deliveries.count.should == 1
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+    end
+  end
+
+  context 'cost interpolation' do
+    let(:email) { FactoryGirl.build(:en_email, subject: '{{cost}}', body: '{{cost}}') }
+    [
+        # site cost
+        ['de', '1,23 €'],
+        ['uk', '£1.23']
+    ].each do |(site, cost)|
+      it "should have #{site} #{cost}" do
+        Site.stub(:name).and_return(site)
+        expect(subject.subject).to eq(cost)
+        expect(subject.body).to eq(cost)
+      end
     end
 
-    context "with options" do
-      let(:from) { 'haha@override.com' }
-      subject { Notifier.registration_with_template(registration, email, from: from) }
+  end
 
-      its(:from) { should == [from] }
-      its(:bcc) { should == [from] }
+  context 'registration interpolation' do
+    [[:person_name, :full_name],
+     [:account_name, :bank_account_name],
+     [:event_name, :event_name],
+     [:registration_code, :registration_code],
+     [:iban, :iban],
+     [:bic, :bic]
+    ].each do |(liquid_name, name)|
+      context "#{name} interpolation" do
+        let(:email) { FactoryGirl.build(:en_email, :subject => "subject {{#{liquid_name}}}", body: "body {{#{liquid_name}}}") }
+        its(:subject) { should match("subject #{registration.send(name)}") }
+        its(:encoded) { should match("body #{registration.send(name)}") }
+      end
     end
   end
 end
