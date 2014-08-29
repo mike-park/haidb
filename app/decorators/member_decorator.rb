0bb437aa3ce83ad0c43bd28e8a46ae550d8d5a47
Mike Park <mikep@quake.net>
2014-08-29 15:32:27 +0200

    rework display of member info

--- app/decorators/member_decorator.rb
+++ app/decorators/member_decorator.rb
@@ -5,16 +5,24 @@ class MemberDecorator < Draper::Decorator
     object.status unless object.status == Membership::EXPERIENCED
   end
 
+  def created_at
+    h.localize(object.created_at.to_date, format: :sortable)
+  end
+
+  def gender
+    object.gender.first
+  end
+
+  def total_on_team
+    object.membership.on_team.count
+  end
+
   def highest_level
-    object.angel.highest_level
+    object.angel.highest_level if team.event.level && object.angel.highest_level < team.event.level
   end
 
-  def most_recently_on_team
+  def last_team_date
     registration = object.membership.on_team.first
-    registration ? "#{h.time_ago_in_words(registration.start_date)} ago" :  'Never'
-  end
-
-  def summary
-    [status, "#{most_recently_on_team} on team", notes].reject(&:blank?).join(', ')
+    h.time_ago_in_words(registration.start_date) if registration
   end
 end
